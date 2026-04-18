1352321b019bc0e190f15f0ca329e544643ff5fe
Devanshu Mishra <devmishra2202@gmail.com>
2026-04-18 12:49:38 +0530

    Added game state using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  const [currentWord] = useState("REACT");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
   return (
     <div>
       <Header />
