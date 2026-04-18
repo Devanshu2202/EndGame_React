322b07aafc8f19fb7efe88d6dd95b1eb7cf31f0a
Devanshu Mishra <devmishra2202@gmail.com>
2026-04-18 12:45:03 +0530

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="text-center mb-8">
+      <h1 className="text-3xl font-bold">Assembly: Endgame</h1>
+      <p className="text-gray-400 mt-2">
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
